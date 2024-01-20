6f91af6c101a2aca4c532e8c38d764f717fd946b
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-19 18:36:36 -0600

    Added an import of makePottery() from potteryWheel.js to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,19 @@
 // Imports go first
-
+import { makePottery } from "./potteryWheel.js"
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 2, 3)
+let cup = makePottery("cup", 1, 4)
+let bowl = makePottery("bowl", 4, 2)
+let plate = makePottery("plate", 3, 1)
+let vase = makePottery("vase", 6, 8)
 
+console.log(mug)
+console.log(cup)
+console.log(bowl)
+console.log(plate)
+console.log(vase)
 
 // Fire each piece of pottery in the kiln
 
