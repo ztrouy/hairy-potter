a3e9676f102060c2e194e9120a3410eed5fefcba
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-19 23:25:25 -0600

    Created potteryList.js and potteryList()

---
+++ src/scripts/potteryList.js
@@ -0,0 +1,22 @@
+// Creates HTML representation of pottery objects available to sell
+export const potteryList = (inventory) => {
+    // Sets up counter for use during iterations
+    let counter = 1
+    
+    // Initializes HTML String to return
+    let htmlString = ""
+
+    // Iterates through each object of the passed in array to generate HTML
+    for (const item of inventory) {
+        htmlString += `
+        <section class="pottery" id="pottery--${counter}"></section>
+            <h2 class="pottery__shape">${item.shape}</h2>
+            <div class="pottery__properties">
+                Item weights ${item.weight} grams and is ${item.height} cm in height
+            </div>
+            <div class="pottery__price">Price is $${item.price}</div>
+        </section>`
+    }
+
+    return htmlString
+}
