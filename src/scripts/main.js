2918f93ebb3793d9f63e693124829c96b052c1e8
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-19 22:40:24 -0600

    imported firePottery into main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Imports go first
 import { makePottery } from "./potteryWheel.js"
+import { firePottery } from "./kiln.js"
 
 
 // Make 5 pieces of pottery at the wheel
@@ -16,7 +17,17 @@ console.log(plate)
 console.log(vase)
 
 // Fire each piece of pottery in the kiln
+mug = firePottery(mug, 2100)
+cup = firePottery(cup, 2300)
+bowl = firePottery(bowl, 2200)
+plate = firePottery(plate, 2000)
+vase = firePottery(vase, 2250)
 
+console.log(mug)
+console.log(cup)
+console.log(bowl)
+console.log(plate)
+console.log(vase)
 
 // Determine which ones should be sold, and their price
 
